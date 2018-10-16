4395fc681815591320e7463ea973940bc74ed7ae
ixAndy1 <andyn11bdf@gmail.com>
2018-10-16 18:07:17 -0400

    Create install_mpi.sh

---
+++ scripts/install_mpi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -x
+sudo yum -y group install "Development Tools"
+cd /software
+sudo wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.2.tar.gz
+sudo tar xzf openmpi-3.1.2.tar.gz
+cd openmpi-3.1.2
+sudo ./configure --prefix=/software
+sudo make
+sudo make all install
+echo "export PATH='$PATH:/software'" >> /users/AN899916/.bashrc
+echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software'" >> /users/AN899916/.bashrc
+cd ..
+sudo rm -Rf openmpi-3.1.2
+sudo rm -Rf openmpi-3.1.2.tar.gz
