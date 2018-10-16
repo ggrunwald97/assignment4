303b0f278af41b06a36625dc0517af4a74d4373b
ixAndy1 <andyn11bdf@gmail.com>
2018-10-16 18:06:33 -0400

    Create mph_path_setup.sh

---
+++ scripts/mph_path_setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "export PATH='$PATH:/software/bin'" >> /users/AN899916/.bashrc
+echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/lib/'" >> /users/AN899916/.bashrc
+source /users/AN899916/.bashrc
